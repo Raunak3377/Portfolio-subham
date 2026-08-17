c7833d2a0562b5cfdb97019628340380b7272aff
ShubhamFlims <shubhammaner1235@gmail.com>
2026-08-18 00:17:17 +0530

    Enable liquid interaction layer

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@
 
 import { CustomCursor } from './components/CustomCursor';
 import { SmoothScroll } from './components/SmoothScroll';
+import { LiquidInteraction } from './components/LiquidInteraction';
 import { Navbar } from './components/Navbar';
 import { Hero } from './components/Hero';
 import { Portfolio } from './components/Portfolio';
@@ -17,6 +18,7 @@ import { Contact } from './components/Contact';
 export default function App() {
   return (
     <SmoothScroll>
+      <LiquidInteraction />
       <CustomCursor />
       <Navbar />
       <main>

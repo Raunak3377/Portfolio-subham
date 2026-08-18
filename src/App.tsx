ce13b77ac7bd681755f6533b5c36559a2dded62d
ShubhamFlims <shubhammaner1235@gmail.com>
2026-08-18 17:44:58 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,18 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { CustomCursor } from './components/CustomCursor';
-import { SmoothScroll } from './components/SmoothScroll';
-import { LiquidInteraction } from './components/LiquidInteraction';
-import { Navbar } from './components/Navbar';
-import { Hero } from './components/Hero';
-import { Clients } from './components/Clients';
-import { Portfolio } from './components/Portfolio';
-import { Services } from './components/Services';
-import { About } from './components/About';
-import { Experience } from './components/Experience';
-import { Skills } from './components/Skills';
-import { Contact } from './components/Contact';
+import { CustomCursor } from "./components/CustomCursor";
+import { SmoothScroll } from "./components/SmoothScroll";
+import { LiquidInteraction } from "./components/LiquidInteraction";
+import { Navbar } from "./components/Navbar";
+import { Hero } from "./components/Hero";
+import { Clients } from "./components/Clients";
+import { Portfolio } from "./components/Portfolio";
+import { Services } from "./components/Services";
+import { About } from "./components/About";
+import { Experience } from "./components/Experience";
+import { Skills } from "./components/Skills";
+import { Contact } from "./components/Contact";
 
 export default function App() {
   return (
